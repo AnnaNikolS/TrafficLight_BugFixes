721b9eff510b4380eec8db822eb04ac0f9f8b36d
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-06 02:35:18 +0300

    writing logic

--- TrafficLight_BugFixes/ViewController.swift
+++ TrafficLight_BugFixes/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class ViewController: UIViewController {
+    
+    enum CurrentLight {
+        case red, yellow, green
     }
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var currentLight = CurrentLight.red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        redLight.layer.cornerRadius = redLight.frame.size.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        startButton.layer.cornerRadius = startButton.frame.size.height / 4
+    }
 
+    @IBAction func pressedStartButton(_ sender: UIButton) {
+        
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        case .yellow:
+            currentLight = .green
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        case .green:
+            currentLight = .red
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        }
+    }
 }
 
